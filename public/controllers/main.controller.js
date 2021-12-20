d49c3ce59ae2eb60c7ea23fce64f151e399cfad9
AlphanetRA <alphanetra0321@gmail.com>
2021-12-21 05:46:21 +0900

    Fixed ESLint issue

--- public/controllers/main.controller.js
+++ public/controllers/main.controller.js
@@ -1,9 +1,9 @@
-var app = angular.module('myfirstmean',[]);
+var app = angular.module('myfirstmean', []);
 
-app.controller('addcontrolle',function($scope,$http){
+app.controller('addcontrolle', function($scope, $http) {
 
-  var refresh = function(){
-    $http.get('/contactlists').then(function(response){
+  var refresh = function() {
+    $http.get('/contactlists').then(function(response) {
       $scope.contactlist = response.data;
       $scope.contact.name = '';
       $scope.contact.email = '';
@@ -12,30 +12,30 @@ app.controller('addcontrolle',function($scope,$http){
 
  refresh();
   //
-  $scope.addData = function(){
-    $http.post("/contactlists",$scope.contact).then(function(response){
+  $scope.addData = function() {
+    $http.post("/contactlists", $scope.contact).then(function(response) {
       console.log(response);
       refresh();
     })
   };
 
-  $scope.remove = function(id){
-    $http.delete('/contactlists/'+id).then(function(response){
+  $scope.remove = function(id) {
+    $http.delete('/contactlists/' + id).then(function(response) {
       refresh();
     })
   };
 
-  $scope.edit = function(){
+  $scope.edit = function() {
     //console.log($scope.contact._id);
-    $http.put('/contactlists/'+$scope.contact._id, $scope.contact).then(function(response){
+    $http.put('/contactlists/' + $scope.contact._id, $scope.contact).then(function(response) {
       refresh();
     });
   };
 
-  $scope.update = function(id){
-    $http.get('/contact/'+id).then(function(response){
+  $scope.update = function(id) {
+    $http.get('/contact/' + id).then(function(response) {
       //console.log(response);
-      $scope.contact= response.data;
+      $scope.contact = response.data;
     });
   };
 
