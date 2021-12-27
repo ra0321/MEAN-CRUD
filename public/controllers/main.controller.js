da05211543af68bc0c24036156f0a8a9c65f7daf
AlphanetRA <alphanetra0321@gmail.com>
2021-12-28 01:28:07 +0900

    fixed format issue

--- public/controllers/main.controller.js
+++ public/controllers/main.controller.js
@@ -1,9 +1,9 @@
 var app = angular.module('myfirstmean', []);
 
-app.controller('addcontrolle', function($scope, $http) {
+app.controller('addcontrolle', function ($scope, $http) {
 
-  var refresh = function() {
-    $http.get('/contactlists').then(function(response) {
+  var refresh = function () {
+    $http.get('/contactlists').then(function (response) {
       $scope.contactlist = response.data;
       $scope.contact.name = '';
       $scope.contact.email = '';
@@ -12,31 +12,28 @@ app.controller('addcontrolle', function($scope, $http) {
 
   refresh();
   //
-  $scope.addData = function() {
-    $http.post("/contactlists", $scope.contact).then(function(response) {
+  $scope.addData = function () {
+    $http.post("/contactlists", $scope.contact).then(function (response) {
       console.log(response);
       refresh();
     })
   };
 
-  $scope.remove = function(id) {
-    $http.delete('/contactlists/' + id).then(function(response) {
+  $scope.remove = function (id) {
+    $http.delete('/contactlists/' + id).then(function (response) {
       refresh();
     })
   };
 
-  $scope.edit = function() {
-    //console.log($scope.contact._id);
-    $http.put('/contactlists/' + $scope.contact._id, $scope.contact).then(function(response) {
+  $scope.edit = function () {
+    $http.put('/contactlists/' + $scope.contact._id, $scope.contact).then(function (response) {
       refresh();
     });
   };
 
-  $scope.update = function(id) {
-    $http.get('/contact/' + id).then(function(response) {
-      //console.log(response);
+  $scope.update = function (id) {
+    $http.get('/contact/' + id).then(function (response) {
       $scope.contact = response.data;
     });
   };
-
 });
